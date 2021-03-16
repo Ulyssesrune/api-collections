5bc97513f92167a86019ca8c5500b75ebf39087e
Your Name <mattuti.mc@gmail.com>
2021-03-16 12:01:16 -0300

    created TesteOperacoes.kt

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -0,0 +1,32 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for (salario in salarios) {
+        println(salario)
+    }
+
+    println("----------------------------------")
+    println("Maior Salario: ${salarios.maxOrNull()}")
+    println("Menor Salario: ${salarios.minOrNull()}")
+    println("Média Salarial: ${salarios.average()}")
+
+    val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
+    println("----------------------------------")
+    salariosMaiorQue2500.forEach { println(it) }
+
+    println("----------------------------------")
+    println(salarios.count { it in 2000.0..5000.0 }) //2.000 a 5.000
+
+    println("----------------------------------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 900.0 })
+
+    println("----------------------------------")
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 900.0 })
+
+
+
+}
